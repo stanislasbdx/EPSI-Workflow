f327f53b68ca9d797db9f8728f1f5d40c4f8d657
Stanislas Castaybert <perso@stan1712.com>
2021-10-15 16:59:06 +0200

    ✨ feature・Ajout de la classe Player

---
+++ C#/ChifoumiClasses/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChifoumiClasses
+{
+    public class Player
+    {
+		private int score;
+
+		public Player()
+        {
+            score = 0;
+        }
+        
+        public int getScore
+        {
+            get => score;
+        }
+		public void setScore(int newScore)
+        {
+            score = newScore;
+        }
+	}
+}
