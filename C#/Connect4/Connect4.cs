6a98ead68aa6f7b7539ee9d28ac31c90df0061cf
Stanislas Castaybert <perso@stan1712.com>
2021-10-21 10:01:28 +0200

    ✨ feature・Init Connect4

---
+++ C#/Connect4/Connect4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Connect4
+{
+    class Connect4
+    {
+        // TODO : Compléter cette classe pour implémenter les règles du puissance 4
+        // Etape 1
+        public int LineCount
+        {
+            get { return 2; }
+        }
+        public int ColCount => 3;
+
+        // Etape 2
+        // TODO : Utiliser un tableau à deux dimensions
+        public char GetPawn(int col, int line) => ' ';
+
+        // Etape 3
+        // Remplir la colonne jouée et changer de joueur
+        public int PlayerNumber => 1;
+
+        public void Play(int column)
+        {
+
+        }
+
+        // Etape 4 
+        // Améliorer le Play pour qu'il détecte la victoire/nul et implémenter Winner et Ended
+        public int Winner => 0;
+
+        public bool Ended => false;
+    }
+}
---
+++ C#/Connect4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Connect4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Connect4();
+
+            do
+            {
+                Display(game);
+                for (; ; )
+                {
+                    Console.WriteLine($"Player {game.PlayerNumber} : Which column 1-{game.ColCount} ?");
+
+                    var turn = Console.ReadLine();
+                    int column;
+
+                    if (int.TryParse(turn, out column))
+                    {
+                        game.Play(column);
+                        break;
+                    }
+                    Console.Error.WriteLine("Invalid column number.");
+                }
+            }
+            while (!game.Ended);
+            Display(game);
+            if(game.Winner == 0)
+            {
+                Console.WriteLine("Draw");
+            }
+            else
+            {
+                Console.WriteLine($"Player {game.Winner} wins.");
+            }
+        }
+        private static void Display(Connect4 game)
+        {
+            for (int y = 0; y < game.LineCount; y++)
+            {
+                for (int x = 0; x < game.ColCount; x++)
+                {
+                    Console.Write($"| {game.GetPawn(x, y)} ");
+                }
+                Console.WriteLine("|");
+                for (int x = 0; x < game.ColCount; x++)
+                {
+                    Console.Write($"+---");
+                }
+                Console.WriteLine("+");
+            }
+            for (int x = 0; x < game.ColCount; x++)
+            {
+                Console.Write($"  {x + 1} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
