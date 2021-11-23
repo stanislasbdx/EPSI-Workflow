abab5797c2194dcadacceeca90b0c1087a820d9f
Stanislas Castaybert <perso@stan1712.com>
2021-11-23 22:48:23 +0100

    ✨ feature・Ajout de classes à l'échiquier (Atelier 2)

--- Python/Echiquier/program.py
+++ Python/Echiquier/program.py
@@ -1,42 +1,64 @@
 TAILLE = 8
 THEME_CLAIR = 0
-LETTRES = "PRNBQK"
-PIECES = [ "♟♜♞♝♛♚", "♙♖♘♗♕♔" ]
 CASES = [" ", "█"]
 
-def vide(couleur):
-    return [('',)] * TAILLE
+class Pions:
+    PIECES = [ "♟♜♞♝♛♚", "♙♖♘♗♕♔" ]
+    LETTRES = "PRNBQK"
+    TAILLE = 8
+    
+    def __init__(self, taille):
+        self.TAILLE = taille
+ 
+    def pieces(self, couleur):
+        SEQUENCE = self.LETTRES[1:]
+        return [ (SEQUENCE[i if i<len(SEQUENCE) else self.TAILLE-i-1], couleur)
+                for i in range(self.TAILLE)
+        ]
+    
+    def get_pions(self, couleur):
+        return [('P', couleur)] * self.TAILLE
+    
+    def get_pieces(self):
+        return [ 
+            { self.LETTRES[j] : self.PIECES[i][j] for j in range(len(self.LETTRES)) }    
+            for i in range(2)
+        ]
 
-def pions(couleur):
-    return [('P', couleur)] * TAILLE
 
-def pieces(couleur):
-    SEQUENCE = LETTRES[1:]
-    return [ (SEQUENCE[i if i<len(SEQUENCE) else TAILLE-i-1], couleur)
-             for i in range(TAILLE)
-    ]
+class Plateau:
+    def __init__(self, Pions):
+        self.echiquier = []
+        self.Pions = Pions
+        
+        constructeursLignes = [ Pions.PIECES, self.pions, self.vide, self.vide ]
 
+        for couleur in range(2):
+            print(couleur)
+            for construireLigne in constructeursLignes:
+                self.echiquier.append(construireLigne(couleur))
+            constructeursLignes.reverse()
 
-echiquier = []
-constructeursLignes = [ pieces, pions, vide, vide ]
+    def pions(couleur):
+        return [('P', couleur)] * Pions.TAILLE
+    
+    def vide(couleur):
+        return [('',)] * Pions.TAILLE
+    
+    def print_echiquier(self):
+        return print(self.echiquier)
 
-for couleur in range(2):
-    for construireLigne in constructeursLignes:
-        echiquier.append(construireLigne(couleur))
-    constructeursLignes.reverse()      
-
-print(echiquier)
-
+    def get_echiquier(self):
+        return self.echiquier
 
 def interligne(ligne, motif0, motif1):
     return "  " + 4 * (motif0 * 4 + motif1 * 4) if ligne%2 == 0 \
-      else "  " + 4 * (motif1 * 4 + motif0 * 4)
+    else "  " + 4 * (motif1 * 4 + motif0 * 4)
 
+Pions = Pions(8)
 
-pieces = [ 
-    { LETTRES[j] : PIECES[i][j] for j in range(len(LETTRES)) }    
-    for i in range(2)
-]
+Plateau = Plateau(Pions)
+echiquier = Plateau.get_echiquier()
 
 sep = interligne(THEME_CLAIR, "▄", " ")
 for y in reversed(range(TAILLE)):
@@ -49,7 +71,8 @@ for y in reversed(range(TAILLE)):
             ligne += bord*4
         else:
             lettre, couleur = echiquier[y][x]
-            ligne += bord + pieces[(THEME_CLAIR + couleur) % 2][lettre] + " " + bord
+            print((THEME_CLAIR + couleur) % 2)
+            ligne += bord + Pions.get_pieces[(THEME_CLAIR + couleur) % 2][lettre] + " " + bord
     print(ligne)
     sep = interligne(y + THEME_CLAIR, "▄", "▀")
 print(interligne(TAILLE + THEME_CLAIR, " ", "▀"))
