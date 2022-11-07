1dc0ba988e98172797fb3be9c227a50699b98961
Stanislas Castaybert <perso@stan1712.com>
2022-11-07 14:18:42 +0100

    🚀 wrkspace upgrade・aCréation du projet avec Maven

---
+++ Java/Bases/src/exercices/poo/Cafe.java
@@ -0,0 +1,2 @@
+package exercices.poo;public class Cafe {
+}
---
+++ Java/Bases/src/exercices/poo/TypeCafe.java
@@ -0,0 +1,11 @@
+package exercices.poo;
+
+public class TypesEnum {
+	public enum TypeCafe {
+		JAVA,
+		MOKA,
+		TYPICA,
+		BOURBON,
+		BATARD
+	}
+}
