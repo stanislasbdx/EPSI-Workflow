9300821ebd2b467d35d626ae9ffc3b90cdbf47c3
Stanislas Castaybert <perso@stan1712.com>
2023-01-16 09:57:49 +0100

    Spring project initialization

---
+++ Java/Spring/src/test/java/fr/stan1712/epsi/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.stan1712.epsi.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
