1f60d187fe7260f5229e0a9f6347719d6f95cc05
Stanislas Castaybert <perso@stan1712.com>
2023-02-24 16:06:07 +0100

    ✨ feature・Partie 5 : POO

--- Javascript/ControleJS&Angular/examen_typescript/script.js
+++ Javascript/ControleJS&Angular/examen_typescript/script.js
@@ -1,14 +1,19 @@
 "use strict";
-const examDiv = document.getElementById("exam-div");
-const hwTitle = document.getElementsByTagName("h1")[0];
+/*
+? Partie: 1 : JS
+const examDiv = document.getElementById("exam-div")! as HTMLElement;
+const hwTitle = document.getElementsByTagName("h1")[0]! as HTMLElement;
 const bodyStyle = document.body.style;
+
 bodyStyle.background = "black";
+
 examDiv.style.width = "450px";
 examDiv.style.height = "450px";
 examDiv.style.margin = "auto";
 examDiv.style.background = "white";
+
 hwTitle.style.textAlign = "center";
-hwTitle.style.fontFamily = "Helvetica";
+hwTitle.style.fontFamily = "Helvetica";*/
 /*
     ? Partie 2 :
 
@@ -25,17 +30,48 @@ hwTitle.style.fontFamily = "Helvetica";
         hwTitle.style.color = "black";
     }
 }*/
+/*
+? Partie 2.5: Event JS
 function clickDiv() {
     const divBgColor = bodyStyle.background;
+
     invertColors(divBgColor);
-}
+}*/
 /**
  * Invert colors from a previous one
  * @param prevColor Previous color used
  */
-function invertColors(prevColor) {
+/*
+? Partie 4: Programmation Fonctionnelle
+function invertColors(prevColor: string) {
     const newColor = prevColor === "white" ? "black" : "white";
+
     bodyStyle.background = newColor;
     examDiv.style.background = prevColor;
     hwTitle.style.color = newColor;
+}*/
+class Page {
+    constructor() {
+        this._div = document.getElementById("exam-div");
+        this._title = document.getElementsByTagName("h1")[0];
+        this._body = document.body;
+        this._body.style.background = "black";
+        this._div.style.width = "450px";
+        this._div.style.height = "450px";
+        this._div.style.margin = "auto";
+        this._div.style.background = "white";
+        this._title.style.textAlign = "center";
+        this._title.style.fontFamily = "Helvetica";
+        this._title.style.background = "white";
+        this._div.addEventListener("click", () => {
+            this.InvertColors(this._body);
+            this.InvertColors(this._div);
+            this.InvertColors(this._title);
+        });
+    }
+    InvertColors(el) {
+        el.style.background = el.style.background === "white" ? "black" : "white";
+        el.style.color = el.style.background === "white" ? "black" : "white";
+    }
 }
+new Page();
--- Javascript/ControleJS&Angular/examen_typescript/script.ts
+++ Javascript/ControleJS&Angular/examen_typescript/script.ts
@@ -1,5 +1,7 @@
 "use strict";
 
+/*
+? Partie: 1 : JS
 const examDiv = document.getElementById("exam-div")! as HTMLElement;
 const hwTitle = document.getElementsByTagName("h1")[0]! as HTMLElement;
 const bodyStyle = document.body.style;
@@ -12,7 +14,7 @@ examDiv.style.margin = "auto";
 examDiv.style.background = "white";
 
 hwTitle.style.textAlign = "center";
-hwTitle.style.fontFamily = "Helvetica";
+hwTitle.style.fontFamily = "Helvetica";*/
 
 /*
 	? Partie 2 :
@@ -31,20 +33,62 @@ hwTitle.style.fontFamily = "Helvetica";
 	}
 }*/
 
+/*
+? Partie 2.5: Event JS
 function clickDiv() {
 	const divBgColor = bodyStyle.background;
 
 	invertColors(divBgColor);
-}
+}*/
 
 /**
  * Invert colors from a previous one
  * @param prevColor Previous color used
  */
+/*
+? Partie 4: Programmation Fonctionnelle
 function invertColors(prevColor: string) {
 	const newColor = prevColor === "white" ? "black" : "white";
 
 	bodyStyle.background = newColor;
 	examDiv.style.background = prevColor;
 	hwTitle.style.color = newColor;
+}*/
+
+class Page {
+	private _body: HTMLElement;
+	private _div: HTMLElement;
+	private _title: HTMLElement;
+
+	constructor() {
+		this._div = document.getElementById("exam-div")! as HTMLElement;
+		this._title = document.getElementsByTagName("h1")[0]! as HTMLElement;
+		this._body = document.body;
+
+
+		this._body.style.background = "black";
+
+		this._div.style.width = "450px";
+		this._div.style.height = "450px";
+		this._div.style.margin = "auto";
+		this._div.style.background = "white";
+
+		this._title.style.textAlign = "center";
+		this._title.style.fontFamily = "Helvetica";
+		this._title.style.background = "white";
+
+
+		this._div.addEventListener("click", () => {
+			this.InvertColors(this._body);
+			this.InvertColors(this._div);
+			this.InvertColors(this._title);
+		});
+	}
+
+	InvertColors(el: HTMLElement) {
+		el.style.background = el.style.background === "white" ? "black" : "white";
+		el.style.color = el.style.background === "white" ? "black" : "white";
+	}
 }
+
+new Page();
